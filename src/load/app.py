c8ef784bf6492c21b87859b22fb0d886d8b386af
ayouberrak2 <ayouberrak318@gmail.com>
2026-09-15 16:55:55 +0100

    feat: add data loading

---
+++ src/load/app.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import psycopg2
+
+df = pd.read_csv("data/gold/final_data.csv")
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="pipeline_api_open_meteo",
+    user = "postgres",
+    password = "12341234",
+    port=5432
+    )
+
+cursor = conn.cursor()
+
+for _, row in df.iterrows():
+
+    cursor.execute(
+        "SELECT id FROM city WHERE name = %s",(row["city"],)
+    )
+
+    res = cursor.fetchone()
+
+    if res :
+        city_id = res[0]
+    else :
+        cursor.execute(
+            "INSERT INTO city (" \
+            "name,latitude,longitude" \
+            ")" \
+            "VALUES (%s,%s,%s)" \
+            "RETURNING id",
+            (
+                row["city"],
+                row["latitude"],
+                row["longitude"]
+            )
+        )
+
+        city_id = cursor.fetchone()[0]
+
+    cursor.execute(
+        
+        "INSERT INTO weather (" \
+        "city_id,date,temperature_max,temperature_min,temperature_moyenne,precipitation,probabilite_pluie,vent,risk_score,risk_level,meteo_code" \
+        ")" \
+        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s)" \
+        "RETURNING id"
+        ,
+        (
+            city_id,
+            row["date"],
+            row["temperature_max"],
+            row["temperature_min"],
+            row["temperature_moyenne"],
+            row["precipitation"],
+            row["probabilite_pluie"],
+            row["vent"],
+            row["risk_score"],
+            row["risk_level"],
+            row["meteo_code"]
+        )
+    )
+
+conn.commit()
+cursor.close()
+conn.close()
+
+
